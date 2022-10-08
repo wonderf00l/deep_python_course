7028b418ed319d807d1bb6fdaf7e9c2a2c431e70
wonderf00l <105116952+wonderf00l@users.noreply.github.com>
2022-10-08 20:15:00 +0300

    Update hw3.py

--- 03/hw3.py
+++ 03/hw3.py
@@ -1,3 +1,22 @@
+from itertools import zip_longest
+
+
+class CustomListIterator:
+    def __init__(self, custom_lst):
+        self.lst = custom_lst
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.lst):
+            val = self.lst[self.index]
+            self.index += 1
+            return val
+        raise StopIteration("Итератор кастомного списка исчерпан")
+
+
 class CustomList(list):
     def __init__(self, *data):
         """Инициализация поля data с извлечением и фильтрацией данных:
@@ -8,7 +27,11 @@ class CustomList(list):
         def construction(val):
             flag = 0
             for item in val:
-                if isinstance(item, (str, dict, bool)):
+                if isinstance(item, dict):
+                    for inner_item in list(item.items()):
+                        construction(inner_item)
+                    continue
+                if isinstance(item, (str, bool)):
                     continue
                 if isinstance(item, (int, float)):
                     self.__data.append(item)
@@ -20,85 +43,97 @@ class CustomList(list):
 
         construction(data)
         self.__len = len(self.__data)
-        super().__init__(self)
+
+    def __iter__(self):
+        return CustomListIterator(self.__data)
 
     def sum(self):
-        return sum(self.__data)
+        return sum(self) 
 
     def __add__(self, other):
-        list_ = other._CustomList__data
-        l_1 = self.__len
-        l_2 = len(list_)
-        if min(l_1, l_2) == l_1:
-            return CustomList([sum(item) for item in zip(self.__data, list_)] + list_[l_1:])
-        return CustomList([sum(item) for item in zip(self.__data, list_)] + self.__data[l_2:])
+        return CustomList([sum(item) for item in zip_longest(self, other, fillvalue=0)])
 
     def __sub__(self, other):
-        list_ = other._CustomList__data
-        l_1 = len(self.__data)
-        l_2 = len(list_)
-        if min(l_1, l_2) == l_1:
-            return CustomList([item[0] - item[1] for item in zip(self.__data, list_)] + list_[l_1:])
-        return CustomList([item[0] - item[1] for item in zip(self.__data, list_)] + self.__data[l_2:])
+        return CustomList([item[0] - item[1] for item in zip_longest(self, other, fillvalue=0)])
 
     def __iadd__(self, other):
-        if len(other) < self.__len:
-            for i in range(len(other)):
-                self.__data[i] += other[i]
-        else:
-            for j in range(self.__len):
-                self.__data[j] += other[j]
-            self.__data += other[self.__len:]
+        self.__data = [sum(item) for item in zip_longest(self, other, fillvalue=0)]
         return self
 
     def __isub__(self, other):
-        if len(other) < self.__len:
-            for i in range(len(other)):
-                self.__data[i] -= other[i]
-        else:
-            for j in range(self.__len):
-                self.__data[j] -= other[j]
-            self.__data += other[self.__len:]
+        self.__data = [item[0] - item[1] for item in zip_longest(self, other, fillvalue=0)]
         return self
 
+    # старая реализация с присоединением оставшейся части большего списка
+    # def __isub__(self, other):
+    #     len_ = len(self)
+    #     if len(other) < len_:
+    #         for i in range(len(other)):
+    #             self.__data[i] -= other[i]
+    #     else:
+    #         for j in range(len_):
+    #             self.__data[j] -= other[j]
+    #         self.__data += other[len_:]
+    #     return self
+
     def __radd__(self, other):
-        if len(other) < self.__len:
-            for i in range(len(other)):
-                other[i] += self.__data[i]
-            return other + self.__data[len(other):]
-        for j in range(self.__len):
-            other[j] += self.__data[j]
+        other = [sum(item) for item in zip_longest(self, other, fillvalue=0)]
         return other
 
     def __rsub__(self, other):
-        if len(other) < self.__len:
-            for i in range(len(other)):
-                other[i] -= self.__data[i]
-            return other + self.__data[len(other):]
-        for j in range(self.__len):
-            other[j] -= self.__data[j]
+        other = [item[0] - item[1] for item in zip_longest(other, self, fillvalue=0)]
         return other
 
+    # старая реализация с присоединением оставшейся части большего списка
+    # def __rsub__(self, other):
+    #    len_ = len(self)
+    #     if len(other) < len_:
+    #         for i in range(len(other)):
+    #             other[i] -= self.__data[i]
+    #         return other + self.__data[len(other):]
+    #     for j in range(len(self)):
+    #         other[j] -= self.__data[j]
+    #     return other
+
     def __eq__(self, other):
-        return True if self.__len == other.sum() else False
+        return True if sum(self) == sum(other) else False
 
     def __ne__(self, other):
-        return True if self.__len != other.sum() else False
+        return True if sum(self) != sum(other) else False
 
     def __lt__(self, other):
-        return True if self.__len < other.sum() else False
+        return True if sum(self) < sum(other) else False
 
     def __le__(self, other):
-        return True if self.__len <= other.sum() else False
+        return True if sum(self) <= sum(other) else False
 
     def __gt__(self, other):
-        return True if self.__len > other.sum() else False
+        return True if sum(self) > sum(other) else False
 
     def __ge__(self, other):
-        return True if self.__len >= other.sum() else False
+        return True if sum(self) >= sum(other) else False
 
     def __str__(self):
         return f"список:{self.__data}, сумма: {self.sum()}"
 
 
-# a = CustomList([1, [[2, 's', (3, {1: 2}, {4, 5}, [[[True, 'hello', (6, 7)]]])]]], 's', 8)
+a = CustomList([1, [[2, 's', (3, {4: 5}, {6}, [[[True, 'hello', (7)]]])]]], 's', 8, '9', {(9): (10)})
+b = CustomList([1, 1, 1])
+print(a)  # список:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], сумма: 55
+print(b)  # список:[1, 1, 1], сумма: 3
+print(a + b, '\t', b + a)
+print(a - b, '\t', b - a)
+a += b
+print(a)  # список:[2, 3, 4, 4, 5, 6, 7, 8, 9, 10], сумма: 58
+a -= b
+print(a)  # список:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], сумма: 55
+print(a + [1, 1], '\t', [1, 1] + a)
+a -= [1, 1]
+print(a)  # список:[0, 1, 3, 4, 5, 6, 7, 8, 9, 10], сумма: 53
+a -= [1] * 10
+print(a)  # список:[-1, 0, 2, 3, 4, 5, 6, 7, 8, 9], сумма: 43
+c = [1, 1]
+c += a
+print(c)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a < b)
+print(a >= [1, 2])
