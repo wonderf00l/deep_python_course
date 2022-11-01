a7f7bd8e39d1e45acabb83f5598bcc38c8a82af3
wonderf00l <105116952+wonderf00l@users.noreply.github.com>
2022-11-02 00:16:25 +0300

    Update server.py

--- 06/server.py
+++ 06/server.py
@@ -1,15 +1,18 @@
-import requests
-from bs4 import BeautifulSoup
 import getopt
 import sys
 import socket
 import threading
+import queue
 from queue import Queue
+from collections import Counter
+import json
+import requests
+from bs4 import BeautifulSoup
 
 
 class Server:
 
-    def __init__(self):  # port - command line arg
+    def __init__(self):
         self._host = socket.gethostname()
         argv = sys.argv[1:]
         try:
@@ -17,60 +20,104 @@ class Server:
                                  ["port=", "workers="])[0]
         except getopt.GetoptError as error:
             print(error)
-        for opt, arg in opts:
-            if opt in ["-p", "--port"]:
-                self._port = int(arg)
-            if opt in ["-w", "--workers"]:
-                self._workers = int(arg)
-            if opt in ["-k"]:
-                self._k_words = int(arg)
-        self.server_sock = socket.socket(
+        try:
+            for opt, arg in opts:
+                if opt in ["-p", "--port"]:
+                    self._port = int(arg)
+                if opt in ["-w", "--workers"]:
+                    self._workers = int(arg)
+                if opt in ["-k"]:
+                    self._k_words = int(arg)
+            if not self._port or not self._workers or not self._k_words:
+                raise ValueError
+        except ValueError:
+            while True:
+                try:
+                    print("Input correct parameters")
+                    self._port = int(input("port: "))
+                    self._workers = int(input("workers: "))
+                    self._k_words = int(input("number of words: "))
+                    break
+                except ValueError:
+                    continue
+        self._server_sock = socket.socket(
             socket.AF_INET,
             socket.SOCK_STREAM
         )
         while True:
             try:
-                self.server_sock.bind((self._host, self._port))
+                self._server_sock.bind((self._host, self._port))
                 break
             except OSError:
                 print("Address already in use, try another")
                 self._port = int(input("Another port: "))
-        self.server_sock.listen()
-        self.queue = Queue(20)
-        self.sem = threading.Semaphore(self._workers)
+        self._server_sock.listen()
+        self._queue = Queue()
+        self._lock = threading.Lock()
 
-    def serve_client(self=None):
-        client_server_sock, client_addr = self.server_sock.accept()
-        print(f"Got connection with {client_addr}")
-        [threading.Thread(
-            target=Server.request_processing,
-            args=(self,) # another args
-        ).start()
-         for _ in range(self._workers)]
-        try:
-            while True:
-                data = client_server_sock.recv(1024).decode()
-                if not data:
+    def serve_client(self):
+        while True:
+            client_sock, client_addr = self._server_sock.accept()
+            print(f"{client_addr} has connected to the server")
+            stat = Queue()
+            threads = [threading.Thread(
+                target=Server.request_processing,
+                args=(self, client_sock, stat)
+            ) for _ in range(self._workers)]
+            for thread in threads:
+                thread.start()
+            try:
+                while True:
+                    data = ''
+                    while True:
+                        char = client_sock.recv(1).decode()
+                        if char == '\n':
+                            break
+                        if not char:
+                            raise ConnectionError
+                        data += char
+                    print(f"data: {data}")
+                    self._queue.put(data)
+            except ConnectionError:
+                print("No data to receive")
+            for thread in threads:
+                thread.join()
+            client_sock.close()
+            print("End of the session on the server\n")
+
+    def request_processing(self, client_sock, stat):
+        while True:
+            try:
+                url = self._queue.get(timeout=5)
+                with self._lock:
+                    print(f"Thread {threading.get_ident()} has started processing {url}")
+                data = requests.get(url, timeout=8)
+                soup = BeautifulSoup(data.text, "lxml")
+                lst = soup.text.replace('\n', '').split(' ')
+                lst_of_words = [word for word in lst if len(word) > 1]
+                # lst_of_words = filter(self.func, lst)
+                words_rate = dict(Counter(lst_of_words).most_common(self._k_words))
+                json_doc = json.dumps(words_rate)
+                client_sock.sendall(json_doc.encode(encoding="utf-8"))
+                with self._lock:
+                    # client_sock.sendall(json_doc.encode(encoding="utf-8")) # is this thread-safe?
+                    stat.put(json_doc)
+                    print(f"{stat.qsize()} URLs have been processed")
+            except queue.Empty:
+                with self._lock:
+                    print("Queue is empty, no URLs to process")  
+                    print(f" Thread {threading.get_ident()} isn't alive anymore")
                     break
-                print(f"data: {data}")
-                self.queue.put(data) 
+            except ConnectionError:
+                print("Client suddenly closed tne connection")
 
-        except ConnectionResetError:
-            print("Lost connection")
-        print("End of the session")
+    # @staticmethod
+    # def func(word):
+    #     try:
+    #         ord(word)
+    #     except TypeError:
+    #         return True
 
-    def set_connection(self):
-        client_server_sock, client_addr = self.server_sock.accept()
-        print(f"Got connection with {client_addr}")
-        return client_server_sock
-
-    def request_processing(self, url):
-        with self.sem:
-            data = requests.get(url)
-            soup = BeautifulSoup(data.text, "lxml")
-
-    def print_statistics(self):
-        pass
 
 if __name__ == '__main__':
     server = Server()
