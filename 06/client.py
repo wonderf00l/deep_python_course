976282d853d6ffa3ae7406edb82c724cffcd146a
wonderf00l <105116952+wonderf00l@users.noreply.github.com>
2022-11-06 23:19:25 +0300

    Update client.py

--- 06/client.py
+++ 06/client.py
@@ -9,11 +9,19 @@ from time import time
 
 
 class Client:
+
     def __init__(self):
         self._host = socket.gethostname()
         self._port = None
         self._workers = None
         self._file_path = None
+        self._file = None
+        self._queue = Queue()
+        self._lock = threading.Lock()
+        self._is_new = ''
+        self._test = None
+
+    def client_configuration(self, test=("not test", None)):
         try:
             opts = getopt.getopt(sys.argv[1:], "p:w:f:", ["port=", "workers=", "file="])[0]
         except getopt.GetoptError as error:
@@ -50,9 +58,7 @@ class Client:
                     break
                 except FileNotFoundError:
                     continue
-        self._queue = Queue()
-        self._lock = threading.Lock()
-        self._is_new = ''
+        self._test = test
 
     def set_connection(self, client_sock):
         while True:
@@ -78,15 +84,25 @@ class Client:
             )
             start = time()
             self.set_connection(client_sock)
-            print(self._port, self._workers, self._file_path)
-            i_list = iter(self._file.readlines())
-            for string in i_list:
-                if string == '\n':
-                    break
-                for _ in range(0, 100):
+            if self._test[0] == "test":
+                if self._test[1] == "valid":
+                    self._queue.put("https://ru.wikipedia.org/wiki/Python\n")
+                if self._test[1] == "invalid":
+                    self._queue.put("invalid test\n")
+                stat = self.send_url(client_sock)
+                self._file.close()
+                client_sock.close()
+                return stat
+            lst = self._file.readlines()
+            if lst[0][-1] != '\n':
+                lst[0] += '\n'
+            for _ in range(0, 100):
+                self._queue.put(lst[0])
+            if len(lst) > 1:
+                for string in lst[1:]:
+                    if string[-1] != '\n':
+                        string += '\n'
                     self._queue.put(string)
-            for string in i_list:
-                self._queue.put(string)
             if not self._queue.empty():
                 threads = [threading.Thread(
                     target=Client.send_url,
@@ -101,15 +117,16 @@ class Client:
             end = time()
             print(f"End of the session on the client, time: {end - start}\n")
             while True:
-                retry = input("Retry connection? (Y/N)")
-                if retry in ['Y', 'y']:
+                restart = input("Restart connection? (Y/N)")
+                if restart in ['Y', 'y']:
                     while True:
                         self._is_new = input("Would you like to change file? (Y/N)")
                         if self._is_new in ['Y', 'y']:
                             while True:
-                                new_path = input("Input new file name:")
+                                self._file_path = input("Input new file name:")
                                 try:
-                                    self._file = open(new_path, 'r', encoding="utf-8")
+                                    self._file = open(self._file_path, 'r',
+                                                      encoding="utf-8")
                                     break
                                 except FileNotFoundError:
                                     print("Incorrect file name")
@@ -120,7 +137,7 @@ class Client:
                         if self._is_new in ['Y', 'y'] or self._is_new in ['N', 'n']:
                             break
                         continue
-                if retry in ['N', 'n']:
+                if restart in ['N', 'n']:
                     raise ConnectionError("Work of the client has been stopped")
                 if self._is_new in ['Y', 'y'] or self._is_new in ['N', 'n']:
                     break
@@ -131,30 +148,35 @@ class Client:
             try:
                 to_send = self._queue.get(timeout=2)
                 with self._lock:
-                    print(f"Thread {threading.get_ident()} got {to_send} from queue")
+                    print(f"Thread {threading.get_ident()} got {repr(to_send)} "
+                          f"from the queue")
                 client_sock.send(to_send.encode())
                 with self._lock:
                     print(f"Thread {threading.get_ident()} sent url")
                 data = client_sock.recv(
                     1024)
-                with self._lock:
-                    pass
                 decoded_data = json.loads(data.decode())
+                if self._test[0] == "test":
+                    return decoded_data
                 with self._lock:
                     print(f"Thread {threading.get_ident()} received stat:\n{decoded_data}")
-                if not data:
-                    raise ConnectionError
             except queue.Empty:
                 with self._lock:
                     print(f"Thread {threading.get_ident()} status: "
                           f"no URLs left to send, dying")
-                    break
-            except ConnectionError:
+                break
+            except TimeoutError as error:
                 with self._lock:
-                    print(f"Server doesn't send statistics "
-                          f"to thread {threading.get_ident()} anymore")
+                    print(f"{threading.get_ident()} -- {error}")
+                break
+            except json.decoder.JSONDecodeError:
+                with self._lock:
+                    print(f"Thread {threading.get_ident()} "
+                          f"can't decode server info")
+                    print("Decode error")
 
 
 if __name__ == "__main__":
     client = Client()
+    client.client_configuration()
     client.start_interaction()
