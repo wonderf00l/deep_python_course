7f554ee3884e25c21c281ac7007b37713f09b10b
wonderf00l <105116952+wonderf00l@users.noreply.github.com>
2022-11-07 19:10:39 +0300

    Update test.py

--- 06/test.py
+++ 06/test.py
@@ -12,8 +12,6 @@ def test_server_valid_url(self):
     client._workers = 10
     client._file_path = "URL.txt"  # указание любого файла для корректного создания клиента
     client.client_configuration(("test", "valid"))  # алгоритм в случае теста - в start_interaction
-    mock_arg = "unittest: {'https://www.python.org/': " \
-               "{'and': 22, 'Python': 20, 'the': 18, 'to': 15}}"
     self.assertEqual(
         {'https://ru.wikipedia.org/wiki/Python': {'Python': 282,
                                                   'Архивировано': 163,
