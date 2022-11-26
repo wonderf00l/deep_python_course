aa5d68a08a53f4c06a2d5cb2ec18660001f987e6
wonderf00l <105116952+wonderf00l@users.noreply.github.com>
2022-11-26 18:08:38 +0300

    Update and rename test_hw5_LRU.py to test_LRU.py

--- 05/test_LRU.py
+++ 05/test_LRU.py
@@ -17,6 +17,20 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(list(inst.cache.keys()), ['k3', 'k2', 2])
         val_2 = inst['k3']
         self.assertEqual(list(inst.cache.keys()), ['k2', 2, 'k3'])
+        
+    def test_cache_edited(self):
+        inst = LRUCache(1)
+        inst["k1"] = "val1"
+        inst["k2"] = "val2"
+        self.assertEqual([inst["k1"], inst["k2"]], [None, "val2"])
+
+        inst_1 = LRUCache(3)
+        inst_1[1] = '1'
+        inst_1[2] = '2'
+        inst_1[3] = '3'
+        inst_1[1] = "new_val"
+        inst_1[4] = '4'
+        self.assertEqual(inst_1[2], None)
 
 
 if __name__ == '__main__':
