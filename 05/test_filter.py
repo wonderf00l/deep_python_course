8d21c55451d5b0f584f22ee4ef8af658842f08c2
wonderf00l <105116952+wonderf00l@users.noreply.github.com>
2022-11-27 20:38:41 +0300

    Update test_filter.py

--- 05/test_filter.py
+++ 05/test_filter.py
@@ -1,32 +1,43 @@
+import os
 import unittest
-from filter import file_filter
-from io import StringIO
 
 
 class MyTestCase(unittest.TestCase):
-    def test_text_filter(self):
-        file_name = "some_text.txt"
-        file_obj = StringIO(
-            '''
-            qweрозаrty
-            123.321
-            hello-world
-            '''
-        )
-        
+    def test_filter(self):
+        file_name = "test_file.txt"
+        buf = []
+        with open(file_name, "w+", encoding="UTF-8") as file:
+            file.write("а Роза упала на лапу Азора hello 123\n"
+                        "qweрозаrty\n"
+                        "hello1 world\n"
+                        "hEllO wd\n"
+                        "123.321\n"
+                        "hello-world\n"
+                        "13 123 .\n"
+                        "123321\n"
+                        "а Роза упала на лапу Азора hello 123\n"
+                        "kmr;hnglbvbФдцркгдшРФкр\n")
+            file.seek(0)
+            for item in file_filter(file, ["hello", "роза", "123"]):
+                self.assertTrue(id(item) not in buf)
+                buf.append(id(item))
+                self.assertTrue(
+                    item in [
+                        "а Роза упала на лапу Азора hello 123", "hEllO wd", "13 123 ."
+                    ]
+                )
+            print("End of the test for filter with f_obj arg")
+            buf.clear()
         for item in file_filter(file_name, ["hello", "роза", "123"]):
+            self.assertTrue(id(item) not in buf)
+            buf.append(id(item))
             self.assertTrue(
                 item in [
-                    "а Роза упала на лапу Азора", "hEllO wd", "13 123 ."
+                    "а Роза упала на лапу Азора hello 123", "hEllO wd", "13 123 ."
                 ]
             )
-
-        for item in file_filter(file_obj, ["hello", "роза", "123"]):
-            self.assertTrue(
-                item not in [
-                    "qweрозаrty", "123.321", "hello-world"
-                ]
-            )
+        print("End of the test for filter with str arg")
+        os.remove("test_file.txt")
 
 
 if __name__ == '__main__':
