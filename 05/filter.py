5d08d6036559088a6c367d36eefe4baa35e11b1e
wonderf00l <105116952+wonderf00l@users.noreply.github.com>
2022-11-27 20:32:36 +0300

    Update filter.py

--- 05/filter.py
+++ 05/filter.py
@@ -2,38 +2,35 @@ import io
 from functools import singledispatch
 
 
-def processing(f_arg, keywords):
-    return [
-        string.rstrip('\n') for string in f_arg
-        for word in keywords
-        if word.lower() in string.lower().split()
-    ]
-
-
 @singledispatch
-def file_filter(file_arg, keywords):
+def file_filter(file_arg: str, keywords):
     with open(file_arg, 'r', encoding='UTF-8') as text:
-        lst = processing(text, keywords)
-        yield from lst
+        for string in text:
+            for word in keywords:
+                if word.lower() in string.lower().split():
+                    yield string.rstrip('\n')
+                    break
 
 
-@file_filter.register(io.StringIO)
 @file_filter.register(io.TextIOWrapper)
-def _filter(file_obj_arg, keywords):
-    lst = processing(file_obj_arg, keywords)
-    yield from lst
+def _filter(file_obj_arg: io.TextIOWrapper, keywords):
+    for string in file_obj_arg:
+        for word in keywords:
+            if word.lower() in string.lower().split():
+                yield string.rstrip('\n')
+                break
 
 
 if __name__ == "__main__":
     FILE_NAME = "some_text.txt"
-    FILE_1 = "some_text_1.txt"
-    with open(FILE_1, 'r', encoding='UTF-8') as file:
-        file_obj = io.StringIO(file.read())
-    gen_obj = file_filter(FILE_NAME, ['hello', 'роза', '123'])
-    gen_obj_1 = file_filter(file_obj, ['hello', 'роза', '123'])
-    # ggen = gen_exp_filter(FILE_NAME, ['hello', 'роза', '123'])
-    for item in gen_obj:
-        print(f"{FILE_NAME}: {item}")
-    print(f"End for {FILE_NAME}")
+
+    with open(FILE_NAME, 'r', encoding='UTF-8') as file:
+        gen_obj = file_filter(file, ['hello', 'роза', '123'])
+        for item in gen_obj:
+            print(f"{FILE_NAME}: {item}")
+        print(f"End for {FILE_NAME}", end='\n'*2)
+
+    print("Now with str arg: ")
+    gen_obj_1 = file_filter(FILE_NAME, ['hello', 'роза', '123'])
     for item_1 in gen_obj_1:
-        print(f"{FILE_1}: {item_1}")
+        print(f"{FILE_NAME}: {item_1}")
